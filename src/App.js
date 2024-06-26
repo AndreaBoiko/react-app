408ca80a4fb18c0123773582c879ab27e8f51b1c
AndreaBoiko <andriy.boyko35@gmail.com>
2024-06-27 00:45:15 +0200

    add Tailwind to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
-  return <div className="App">Hello</div>;
+  const [role, setRole] = useState('dev');
+  return (
+    <div className="App">
+      <input
+        type="text"
+        onChange={(e) => {
+          setRole(e.target.value)
+        }}
+      />
+      <Employee name="Andrii" role="programmer" />
+      <Employee name="Bogdan" role={role} />
+    </div>
+  );
 }
 
 export default App;
